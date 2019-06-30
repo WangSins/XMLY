a02d8c907b6ff4107571ff9a6a7234d83ee13333
WangSins <WangSins@126.com>
2019-06-30 11:48:32 +0800

    Subscribe page, custom subscription interface.

---
+++ app/src/main/java/com/example/wsins/xmly/interfaces/ISubscriptionPresenter.java
@@ -0,0 +1,30 @@
+package com.example.wsins.xmly.interfaces;
+
+import com.example.wsins.xmly.base.IBasePresenter;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+
+/**
+ * Created by Sin on 2019/6/30
+ * 订阅存在上限，不超过100个
+ */
+public interface ISubscriptionPresenter extends IBasePresenter<ISubscriptionCallback> {
+
+    /**
+     * 添加订阅
+     *
+     * @param album
+     */
+    void addSubscription(Album album);
+
+    /**
+     * 删除订阅
+     *
+     * @param album
+     */
+    void deleteSubscription(Album album);
+
+    /**
+     * 获取订阅列表
+     */
+    void getSubscriptionList();
+}
