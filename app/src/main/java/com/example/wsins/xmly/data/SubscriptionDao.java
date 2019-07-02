c7eba12bca10b3c9c4882b09fac30cb826273bb7
WangSins <WangSins@126.com>
2019-07-03 00:29:45 +0800

    Subscribe to the page, and the list of subscriptions is in reverse order.

--- app/src/main/java/com/example/wsins/xmly/data/SubscriptionDao.java
+++ app/src/main/java/com/example/wsins/xmly/data/SubscriptionDao.java
@@ -102,7 +102,7 @@ public class SubscriptionDao implements ISubDao {
         try {
             db = ximalayaDBHelper.getReadableDatabase();
             db.beginTransaction();
-            Cursor query = db.query(Constants.SUB_TB_NAME, null, null, null, null, null, null);
+            Cursor query = db.query(Constants.SUB_TB_NAME, null, null, null, null, null, "_id desc");
             //封装数据
             while (query.moveToNext()) {
                 Album album = new Album();
